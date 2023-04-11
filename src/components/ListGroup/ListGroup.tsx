6de53c17e2daee317ad97b0512079dac7aeac898
Ceci <followmyfriends32@gmail.com>
2023-04-11 16:13:27 -0400

    Add onclick event to an item on ListGroup component

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,8 +1,12 @@
+import { MouseEvent } from "react";
 import "./ListGroup.css";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+
+  const handleClick = (event: MouseEvent) => {
+    console.log(event);
+  };
 
   return (
     <div className="list-container">
@@ -10,7 +14,7 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, i) => (
-          <li className="list-item" key={i}>
+          <li className="list-item" key={i} onClick={handleClick}>
             {item}
           </li>
         ))}
