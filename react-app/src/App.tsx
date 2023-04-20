e09078477f6c6dd4e6e0e2d0434bde8a4206ac4e
Ceci <followmyfriends32@gmail.com>
2023-04-20 13:27:18 -0400

    Adding Navbar and Cart components

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,14 @@
-import Like from "./components/Like/Like";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
 
 function App() {
+  const [cartItems, setCartItems] = useState(["product-1", "product-2"]);
+
   return (
     <div>
-      <Like onClick={() => console.log("clicked")} />
+      <Navbar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
     </div>
   );
 }
